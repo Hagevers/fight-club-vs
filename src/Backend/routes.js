e4845355bd88b3ad89037c24c11a184229496255
= <amoihadad1@gmail.com>
2022-09-10 15:58:48 +0300

    loginCom

--- src/Backend/routes.js
+++ src/Backend/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const signUpTemplate = require('./SignupModel')
 const bcrypt = require('bcrypt')
 
-router.post('/signUp', async (request,response) => {
+router.post('/signUp', async (request, response) => {
     const saltPass = await bcrypt.genSalt(10)
     const securePass = await bcrypt.hash(request.body.password, saltPass)
     const signUpUser = new signUpTemplate ({
@@ -17,6 +17,26 @@ router.post('/signUp', async (request,response) => {
     })
     .catch(error => {
         response.json(error)
+    })
+})
+
+router.get('/dashboard', (request, response) => {
+    if (!loggedIn){
+        return response.status(401).send();
+    }
+})
+router.post('/signIn', (request, response) => {
+    const Email = request.body.Email
+    const password = request.body.password
+    signUpTemplate.findOne({Email:Email, password:password}, function(err, user){
+        if (err){
+            console.log(err)
+            return response.status(500).send();
+        }
+        if (!user){
+            return response.status(404).send();
+        }
+        return response.status(200).send();
     })
 })
 
--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import '../Styling/LoginPage.css'
 import Logo from '../Styling/logo.png'
 
 function LoginPage (props){
+    const axios = require('axios')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleLoginSubmit = function(e){
+        const loginDetails = {
+            Email: email,
+            password: password
+        }
+        e.preventDefault()
+        axios.post("http://localhost:5000/app/signIn", loginDetails)
+        .then(response => console.log(response.data))
+        setEmail('')
+        setPassword('')
+    }
     return(
-        <form className={"active All-Login"}>
+        <form className={"active All-Login" } onSubmit = {handleLoginSubmit}>
             <div className="Login-Header">
                 <img src={Logo} name="Logo" onClick={props.onLogoClick}/>
             </div>
             <div className="Login-Form">
-                <input type="text" placeholder="שם משתמש" />
-                <input type="password" placeholder="סיסמא" />
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="שם משתמש" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="סיסמא" />
                 <input type="submit" value="התחבר" />
             </div>
             <div className="Forgot">
