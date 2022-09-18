2595ec526fd8526a074b25c1e01fe0451c995d29
= <amoihadad1@gmail.com>
2022-09-18 21:55:17 +0300

    spi

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,31 @@
-export default function hello(req, res) {
-    res.statusCode = 200;
-    res.json({ message: 'It works' });
-  }
+import {MongoClient} from 'mongodb';
+
+const uri = process.env.MONGODB_URI
+const options = {}
+
+if (!uri){
+    throw new Error ("Problem accured")
+}
+
+export default async function handler (request, response){
+    try{
+        const mongo_client = await new MongoClient(uri,options).connect();
+        console.log('connected');
+        const db = mongo_client.db("pmmbrp");
+        const table = mongo_client.collection("pmmbrps");
+        const result = await table
+            .find({})
+            .project({
+                NickName: 0,
+                Password: 0,
+                Email: 0
+
+            })
+            .limit(4)
+            .toArray();
+        response.status(200).json(result);
+    } catch(e){
+        console.log(e)
+        response.status(500).json(e);
+    }
+}
